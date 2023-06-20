66d4087e1a4660b7065bd84333fcac69b64d74c4
alisonBRs <97985844+alisonBRs@users.noreply.github.com>
2023-06-20 19:06:17 -0300

    modal edit working

--- src/components/modal/ModalBox.jsx
+++ src/components/modal/ModalBox.jsx
@@ -1,26 +1,59 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { FaRegEdit } from "react-icons/fa";
+import { useState } from "react";
 
 export function ModalBox({ service, styles, serviceId }) {
+  const [toggle, setToggle] = useState(false);
+
   const deleteService = () => {
     serviceId(service.id);
   };
+
+  const toggleEdit = () => {
+    setToggle(!toggle);
+  };
   return (
     <>
-      <div className={styles.services_descriptions}>
-        <h4>{service.name}</h4>
-        <p>Custo: R${service.cost.toLocaleString("pt-br")}</p>
-        <p>{service.description}</p>
-      </div>
+      {!toggle ? (
+        <>
+          <div className={styles.services_descriptions}>
+            <h4>{service.name}</h4>
+            <p>Custo: R${service.cost.toLocaleString("pt-br")}</p>
+            <p>{service.description}</p>
+          </div>
 
-      <div className={styles.services_btns}>
-        <span className={styles.edit}>
-          <FaRegEdit />
-        </span>
-        <span onClick={deleteService} className={styles.trash}>
-          <RiDeleteBin6Line />
-        </span>
-      </div>
+          <div className={styles.services_btns}>
+            <span onClick={toggleEdit} className={styles.edit}>
+              <FaRegEdit />
+            </span>
+            <span onClick={deleteService} className={styles.trash}>
+              <RiDeleteBin6Line />
+            </span>
+          </div>
+        </>
+      ) : (
+        <div className={styles.edit_services}>
+          <h3>Editar serviço</h3>
+          <form autoComplete="off">
+            <label htmlFor="name">
+              <input placeholder="nome do serviço" type="text" id="name" />
+            </label>
+
+            <label htmlFor="cost">
+              <input placeholder="custo" type="number" id="cost" />
+            </label>
+
+            <label htmlFor="description">
+              <input placeholder="descrição" type="text" id="description" />
+            </label>
+
+            <div className={styles.edit_services_btns}>
+              <button onClick={toggleEdit}>Cancelar</button>
+              <button>Salvar</button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 }
